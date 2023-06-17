ebf04e70d9312098ff7ade83fcfc82e1ff4643ce
36vanquyet <132633695+36vanquyet@users.noreply.github.com>
2023-06-17 07:45:46 +0700

    Delete fiveserver.config.js

--- fiveserver.config.js
+++
@@ -1,6 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-    php: "/usr/bin/php",             // Đường dẫn cho Ubuntu/Linux hoặc macOS
-    php: "C:\\xampp\\php\\php.exe" // Đường dẫn cho Windows
-  }
-  
